3bd582983562c3142348f2e695ba56fcc0337423
Gus <gustavo.canales@me.com>
2024-05-27 21:55:29 +0200

    added mongdb atlas

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const app = require("./app");
 const PORT_SERVER = process.env.PORT || 3977;
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
+// MongoDB connection details
+const isProduction = process.env.NODE_ENV === 'production';
+
+const mongoURI = isProduction
+  ? `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_CLUSTER}/${process.env.MONGO_ATLAS_DBNAME}?retryWrites=true&w=majority`
+  : `mongodb://${IP_SERVER}:${PORT_DB}/netzwerk`;
+
 mongoose.connect(
-  `mongodb://${IP_SERVER}:${PORT_DB}/netzwerk`,
+  mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
   (err, res) => {
     if (err) {
       throw err;
